8a642e90a09b4a795b5eeec0519e3ef739e65b06
JianSanchez <sanchez.jianimmanuel@gmail.com>
2023-05-09 21:37:49 +0800

    fINAL

---
+++ script.js
@@ -0,0 +1,54 @@
+const insert = () => {
+    let type = document.querySelector('#type').value 
+    let label = document.querySelector('#label').value 
+    
+
+    switch(type){
+        case 'text': appendText(label)
+            break
+        case 'button': appendButton(label)
+            break
+        case 'radio': appendRadio(label)
+            break
+    }
+}
+
+const appendText = (label = "No label") => {
+    let el = document.createElement('input')
+
+    el.setAttribute('type', 'text')
+    el.setAttribute('class', 'form-control mt-2')
+    el.setAttribute('placeholder', label)
+
+    document.querySelector('#display').append(el)
+    
+}
+
+const appendButton = (label = "No label") => {
+    let el = document.createElement('button')
+
+    el.setAttribute('type', 'text')
+    el.setAttribute('class', 'btn btn-primary mt-2') 
+    el.textContent = label
+
+    document.querySelector('#display').append(el)
+}
+const appendRadio = (label = "No label") => {
+    let el = document.createElement('input')
+
+    el.setAttribute('type', 'text')
+    el.setAttribute('class', ' mt-2')
+    el.textContent = label
+
+    document.querySelector('#display').append(el)
+}
+const listUl = document.querySelector('.myBtn')
+const lis = listUl.children;
+
+//function createBtn(li) {
+    //const remove = document.createElement('button');
+    //remove.className = ' btn-icon remove';
+    //li.appendChild(remove);
+
+    //return li;
+
